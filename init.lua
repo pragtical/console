66ded63e95aad7be78ae44040fbb5f647a0c0ea0
Logan Forman <35375036+dev-dwarf@users.noreply.github.com>
2024-01-01 15:21:22 -0700

    Work around processes not terminating under Windows (#18)
    * Break run command on empty text.
    
    * And proc running check before breaking
    
    * Update init.lua
    
    Co-authored-by: Guldoman <giulio.lettieri@gmail.com>
    
    ---------
    
    Co-authored-by: Guldoman <giulio.lettieri@gmail.com>

--- init.lua
+++ init.lua
@@ -151,6 +151,8 @@ function console.run(opt)
         push_output(text, opt)
         coroutine.yield(0.1)
         text = proc:read_stdout()
+
+        if text and #text == 0 and not proc:running() then break end
       end
       if output[#output].text ~= "" then
         push_output("\n", opt)
